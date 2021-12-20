fd108e6114d08aa58c60a11f879e78fc5093a3d8
Misha <reddreadthomaas@yandex.ru>
2021-12-20 23:26:07 +1000

    remove Migrations

--- app/src/main/java/ru/fefu/activitytracker/database/ActivitiesDataBase.kt
+++ app/src/main/java/ru/fefu/activitytracker/database/ActivitiesDataBase.kt
@@ -1,6 +1,5 @@
 package ru.fefu.activitytracker.database
 
-import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -8,9 +7,8 @@ import androidx.room.TypeConverters
 
 @TypeConverters(Converters::class)
 @Database(
-    version = 2,
+    version = 2 ,
     entities = [Activity::class],
-    autoMigrations = [AutoMigration (from = 1, to = 2)]
 )
 abstract class ActivitiesDataBase: RoomDatabase() {
     abstract fun activityDao(): ActivityDao
